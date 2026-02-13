6c3d55d3ae73ff238b0a5d12d6ef1ca3c0951fe1
Danil Efimov <defimov@ethz.ch>
2026-02-13 15:52:37 +0100

    dev: Added logging

--- cogitao_wrapper/__init__.py
+++ cogitao_wrapper/__init__.py
@@ -1,5 +1,14 @@
+import logging
+
+if not logging.getLogger().hasHandlers():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
 from .config import GeneratorConfig
 from .generator import DatasetGenerator
 from .dataset import CogitaoDataset
+
 
 __all__ = ["GeneratorConfig", "DatasetGenerator", "CogitaoDataset"]
